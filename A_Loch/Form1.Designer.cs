30f2ee1846f904dba2bc344aaf0e687ef0e3098d
magnus.keller <magnus.keller@B30-PC12>
2022-03-09 12:24:34 +0100

    Klasse Spielleiter unfertig 1

--- A_Loch/Form1.Designer.cs
+++ A_Loch/Form1.Designer.cs
@@ -47,7 +47,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "7";
             this.ResumeLayout(false);
 
         }
---
+++ A_Loch/Spielleiter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Loch
+{
+    public class Spielleiter
+    {
+
+        List<Karte> Kartenstapel;
+        List<Spieler> Spielerliste;
+        List<Spieler> Reihenfolge;
+
+
+
+
+
+
+        public Spielleiter()
+        {
+            Kartenstapel = new List<Karte>();
+            Spielerliste = new List<Spieler>();
+            Reihenfolge = new List<Spieler>();
+        }
+
+        void Spielerhinzufügen(Spieler neuerSpieler)
+        {
+
+        }
+
+        private Kartengenerieren()
+        {
+            return List<Karte>;
+        }
+
+        private Kartenmischeln()
+        {
+            return List<Karte>;
+        }
+
+        public Kartenzuteilen()
+        {
+            //pro Spieler eine Liste
+        }
+
+        public k7Sucher()
+        {
+            return Spieler;
+        }
+
+        public Reihenfolge()
+        {
+            return List<Reihenfolge>;
+        }
+
+        public Rundenende()
+        {
+            return Meme;
+        }
+
+        void Rangliste()
+        {
+           
+        }
+
+
+
+
+
+
+
+    }
+}
