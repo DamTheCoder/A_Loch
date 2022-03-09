55dabb9823d819e523fddd11dde58e062f35a9bc
Raphael <Raphael.Renner171@gmail.com>
2022-03-09 12:17:38 +0100

    Klassen zu Karte geschrieben

---
+++ A_Loch/Hand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Loch
+{
+    internal class Hand : Kartenliste
+    {
+        public override void Add()
+        {
+            base.Add();
+        }
+
+        public override void Clear()
+        {
+            base.Clear();
+        }
+
+    }
+}
---
+++ A_Loch/Karte.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace A_Loch
+{
+    internal class Karte
+    {
+        private int wert;
+        private string farbe;
+
+        public Karte(int wert, string farbe)
+        {
+
+        }
+        public void KarteZeichnen(Graphics g)
+        {
+            
+        }
+    }
+}
