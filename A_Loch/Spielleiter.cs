663bd4cc8d126ebaf6671b829d0b2156ef0e1ed2
simon.karle <simon.karle@B30-PC13>
2022-03-16 12:39:29 +0100

    kartengenerieren fertiggestellt

--- A_Loch/Spielleiter.cs
+++ A_Loch/Spielleiter.cs
@@ -12,8 +12,9 @@ namespace A_Loch
         List<Karte> kartenstapel;
         List<Spieler> spielerliste;
         List<Spieler> reihenfolge;
-        List<Hand> spielerkarten;
+        List<Hand> hand;
         Hand anfangshand;
+        string[] farbe = new string[4];
 
 
 
@@ -25,29 +26,39 @@ namespace A_Loch
             kartenstapel = new List<Karte>();
             spielerliste = new List<Spieler>();
             reihenfolge = new List<Spieler>();
-            spielerkarten = new List<Hand>();
+            hand = new List<Hand>();
+            farbe[0] = "Karo";
+            farbe[1] = "Herz";
+            farbe[2] = "Pik";
+            farbe[3] = "Kreuz";
 
         }
 
         void spielerhinzufügen(Spieler neuerSpieler)
         {
-            spielerliste.Add(new Spieler("Louise1", spielerkarten[0]));
-            spielerliste.Add(new Spieler("Louise2", spielerkarten[1]));
-            spielerliste.Add(new Spieler("Louise3", spielerkarten[2]));
-            spielerliste.Add(new Spieler(Spielername, spielerkarten[3]));
+            spielerliste.Add(new Spieler("Lousie1", hand[0]));
+            spielerliste.Add(new Spieler("Louise2", hand[1]));
+            spielerliste.Add(new Spieler("Louise3", hand[2]));
+            spielerliste.Add(new Spieler("Spielername", hand[3]));
         }
 
         void kartengenerieren()
         {
             anfangshand = new Hand();
-            anfangshand.Add(new Karte(7, "Karo"));
-            anfangshand.Add(new Karte(8, "Karo"));
 
+            for (int f = 0; f < 4; f++)
+            {
+                
+                for (int i = 7; i < 15; i++)
+                {
+                    int u = 0;
+                    String farbeq = farbe[u];
+                    anfangshand.Add(new Karte(i, farbeq));
+                    u++;
 
-
-
-
-
+                }
+            }
+            
 
 
 
@@ -55,15 +66,17 @@ namespace A_Loch
 
         void kartenmischeln()
         {
+
             
         }
 
         public kartenzuteilen()
         {
             //pro Spieler eine Liste
+            return;
         }
 
-        public k7Sucher()
+        public Spieler k7Sucher()
         {
             return Spieler;
         }
