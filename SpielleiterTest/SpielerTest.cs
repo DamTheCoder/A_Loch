5d6e302b011b9ab523c3d1dfe0a1d7ec150d3b8c
Louis Kuhn <louis.kuhn03@gmail.com>
2022-03-23 12:41:32 +0100

    Testklasse Karte

---
+++ SpielleiterTest/SpielerTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using A_Loch;
+
+namespace SpielleiterTest
+{
+    public class Spielertest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Spieler()
+        {
+            Hand h1 = new Hand();
+            Spieler s1 = new Spieler("Jürgen", h1);
+            Assert.Pass();
+        }
+        [Test]
+        public void Hand()
+        {
+            Hand h1 = new Hand();
+            Assert.Pass();
+        }
+        private const int wert = 7;
+        [Test]
+        public void KarteWert()
+        {
+            Karte k7 = new Karte(7, "Karo");
+            k7.getWert();
+            Assert.AreEqual(wert, k7.getWert());
+        }
+        private const string farbe = "Karo";
+        public void KarteFarbe()
+        {
+            Karte k7 = new Karte(7, "Karo");
+            k7.getFarbe();
+            Assert.AreEqual(farbe, k7.getFarbe());
+        }
+    }
+}
