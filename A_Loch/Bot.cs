efe9e2ae3c6420b86707c22ed83cb821e0891c13
Louis Kuhn <louis.kuhn03@gmail.com>
2022-05-25 12:20:22 +0200

    Bot

--- A_Loch/Bot.cs
+++ A_Loch/Bot.cs
@@ -8,19 +8,32 @@ namespace A_Loch
 {
     public class Bot : Spieler
     {
-        public Bot() : base()
-            { 
+        public List<Karte> gewählteKarte;
+        public Bot(string n, Hand h1) : base(n,h1)
+            {
 
             }
         private void karteaussuchen(Ausgespielt a)
         {
-            for (int i = 0; i < hand.count();i++)
+            for (int i = hand.count(); i < 0;i--)
             {
-                if(hand.getKarte(i).getWert() == a.getKarte)
+                if(hand.getKarte(i).getWert() > a.getKarte(1).getWert())
                 {
-
+                    Kartenwert j = hand.getKarte(i).getWert();
+                    if(hand.CountWert(j) >= a.CountWert(j))
+                    {
+                        if (gewählteKarte.Count() < a.count())
+                        {
+                            gewählteKarte.Add(hand.getKarte(i));
+                        }
+                        else
+                        {
+                            this.weiterspielen();
+                        }
+                    }
                 }
             }
+            this.weiterspielen();
         }
     }
 }
--- A_Loch/Hand.cs
+++ A_Loch/Hand.cs
@@ -44,7 +44,5 @@ namespace A_Loch
                                
             }
         }
-
-        
     }
 }
